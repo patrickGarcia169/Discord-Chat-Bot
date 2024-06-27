6bf26181a002c389f04b5415a45f4a3af30454c0
patrickGarcia169 <patrickgar169@gmail.com>
2024-06-27 19:55:18 -0400

    Sends picture, but doesn't refresh. Trying something

--- commands/utilities/picture.js
+++ commands/utilities/picture.js
@@ -1,19 +1,43 @@
 const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require("discord.js");
 
-// Add Picture With Path
-const file = new AttachmentBuilder("./props/Dog.jpg");
-const picture = new EmbedBuilder()
-    .setTitle("Cute Dog")
-    .setImage("attachment://Dog.jpg")
+function objCreation(fileName, title){
+    const file = new AttachmentBuilder(`./props/${fileName}`);
+    const picture = new EmbedBuilder()
+        .setTitle(title)
+        .setImage(`attachment://${fileName}`)
+    
+    return [picture, file];
+};
 
+function randChoice(array){
+    const choices = array.length;
+    const choice = Math.floor(Math.random() * choices);
+
+    const fileName = array[choice][0];
+    const title = array[choice][1];
+
+    return [fileName, title];
+}
+
+// Random Array
+randArr = [
+    ["Dog.jpg", "Cute Dog"], ["Cat.jpg", "Cute Cat"], ["Bird.jpg", "Cute Bird"]
+];
+
+// Choice
+const [fileName, title] = randChoice(randArr);
+
+// Object Creation
+let [picture, file] = objCreation(fileName, title);
 
 module.exports = {
 
     data: new SlashCommandBuilder()
         .setName("picture")
-        .setDescription("Picture of dog"),
+        .setDescription("Picture of animal"),
 
     async execute(interaction){
         await interaction.reply({embeds: [picture], files: [file]});
+        
     },
 };
